fc5eb3ee443c3a4508b26d3040fcd3d4223c5da0
umarkhon <umarkhonakhmedov2000@gmail.com>
2023-03-19 11:17:38 +0500

    Setup Conversation Route and Conversation Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { RegisterPage } from "./pages/RegsiterPage";
 import { LoginPage } from "./pages/LoginPage";
+import { ConversationPage } from "./pages/ConversationPage";
+import { ConversationChannelPage } from "./pages/ConversationChannelPage";
 
 function App() {
   return (
@@ -9,16 +11,8 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="conversations"
-          element={
-            <div>
-              <div>Conversations</div>
-              <Outlet />
-            </div>
-          }
-        >
-          <Route path=":id" element={<div>Conversation ID Page</div>} />
+        <Route path="conversations" element={<ConversationPage />}>
+          <Route path=":id" element={<ConversationChannelPage />} />
         </Route>
       </Routes>
     </>
